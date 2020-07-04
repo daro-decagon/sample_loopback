9a13fcd648a3ec7905a33537f474cf597959407e
dizsart <darogadibia@gmail.com>
2020-07-04 08:57:54 +0100

    make table for the film models

--- create-data-tables.js
+++ create-data-tables.js
@@ -17,4 +17,25 @@ module.exports = function(app) {
       console.log('Models created: \n', People);
     });
   })
+
+
+  app.dataSources.mysqlDs.automigrate('Film', function(err) {
+    if (err) throw err;
+
+    app.models.Film.create([{
+      title: 'A New Hope',
+      opening_crawl: 'It is a period of civil war',
+      director: 'George Lucas',
+      producer: 'Gary Kurtz'
+    }, {
+      title: 'The Empire Strikes Back',
+      opening_crawl: 'It is a dark time for the rebellion',
+      director: 'Irvin Kershner',
+      producer: 'Rick McCallum'
+    }], function(err, People) {
+      if (err) throw err;
+
+      console.log('Models created: \n', People);
+    });
+  });
 };
