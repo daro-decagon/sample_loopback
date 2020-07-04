81de7d98ab4196e27ae9b75f94d37004605890b3
dizsart <darogadibia@gmail.com>
2020-07-04 08:57:17 +0100

    make People data avaliable for the people table

--- create-data-tables.js
+++ create-data-tables.js
@@ -1,7 +1,20 @@
 module.exports = function(app) {
   app.dataSources.mysqlDs.automigrate('People', function(err) {
     if (err) throw err;
+    app.models.People.create([{
+      name: 'Luke Skywalker',
+      height: 172,
+      mass: 77,
+      gender: 'Male'
+    }, {
+      name: 'C-3PO',
+      height: 167,
+      mass: 75,
+      gender: 'Undetermined'
+    }], function(err, People) {
+      if (err) throw err;
 
-
+      console.log('Models created: \n', People);
+    });
   })
 };
